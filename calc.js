43d26c01c3a83d6c58216e7e65218d6beba86925
AdemolaSam <76760844+AdemolaSam@users.noreply.github.com>
2022-05-11 13:55:39 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,6 @@
+//let output = document.getElementsByName('input').value;
+//let button = document.getElementsByName('button').innerText;
+
+/*addEventListener(onclick, function display(num) {
+    output.value += button.innerText;
+})*/
